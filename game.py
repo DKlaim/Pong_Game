a09fb2a022ee513dabdd3c768478222bc8c86671
Denis.K <dk.ptspb@ya.ru>
2024-10-28 21:42:03 +0300

    Модуль_6_Практика_ч3: Игра "Настольный тенис"

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import arcade
+from pyglet.input.evdev_constants import key_raw_names
 
 # Константы (определяются заглавными)
 SCREEN_WIDTH = 800
@@ -8,18 +9,33 @@ SCREEN_TITLE = 'Pong Game'
 
 class Ball(arcade.Sprite):
     def __init__(self):
-        super().__init__('ball.png', 0.08)
-        self.change_x = 1
-        # self.change_y = -1
+        super().__init__('ball.png', 0.1)
+        self.change_x = 3
+        self.change_y = 3
 
     def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+        if self.left <= 0:
+            self.change_x = -self.change_x
+        if self.right >= SCREEN_WIDTH:
+            self.change_x = -self.change_x
+        if self.top >= SCREEN_HEIGHT:
+            self.change_y = -self.change_y
+        if self.bottom <= 0:
+            self.change_y = -self.change_y
 
 
 class Bar(arcade.Sprite):
     def __init__(self):
         super().__init__('bar.png', 1.0)  # передаём в родительский init картинку и её масштаб
+
+    def update(self):
+        self.center_x += self.change_x
+        if self.right >= SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+        if self.left <= 0:
+            self.left = 0
 
 
 # Главное окно игрового поля
@@ -43,7 +59,20 @@ class Game(arcade.Window):
         self.ball.draw()  # отрисовываем мячик в игровом окне
 
     def update(self, delta):
+        if arcade.check_for_collision(self.bar, self.ball):
+            self.ball.change_y = -self.ball.change_y
         self.ball.update()
+        self.bar.update()
+
+    def on_key_press(self, key: int, modifiers: int):
+        if key == arcade.key.LEFT:
+            self.bar.change_x = -5
+        if key == arcade.key.RIGHT:
+            self.bar.change_x = 5
+
+    def on_key_release(self, key: int, modifiers: int):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.bar.change_x = 0
 
 
 if __name__ == '__main__':
